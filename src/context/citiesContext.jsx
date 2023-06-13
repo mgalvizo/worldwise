065a2b368c6e075504e57244e0700c1c0c4603b1
Miguel Garcia <miguelangelgarciaalvizo@hotmail.com>
2023-06-12 22:18:10 -0600

    refactor the context from useState to useReducer, create reducer function

--- src/context/citiesContext.jsx
+++ src/context/citiesContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect, useCallback } from 'react';
+import { createContext, useEffect, useCallback, useReducer } from 'react';
 
 const BASE_URL = 'http://localhost:3001';
 
@@ -6,15 +6,53 @@ const initialValue = { cities: [], isLoading: false };
 
 const CitiesContext = createContext(initialValue);
 
+const initialState = {
+    cities: [],
+    isLoading: false,
+    error: null,
+    currentCity: {},
+};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'LOADING':
+            return { ...state, isLoading: true };
+        case 'CITIES/LOADED':
+            return { ...state, isLoading: false, cities: action.payload };
+        case 'CITY/CREATED':
+            return {
+                ...state,
+                isLoading: false,
+                cities: [...state.cities, action.payload],
+                currentCity: action.payload,
+            };
+        case 'CITY/DELETED':
+            return {
+                ...state,
+                isLoading: false,
+                cities: state.cities.filter(city => city.id !== action.payload),
+                currentCity: {},
+            };
+        case 'CITY/LOADED':
+            return { ...state, isLoading: false, currentCity: action.payload };
+        case 'REJECTED':
+            return { ...state, isLoading: false, error: action.payload };
+        default:
+            throw new Error('Unknown action type.');
+    }
+};
+
 const CitiesContextProvider = ({ children }) => {
-    const [cities, setCities] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [currentCity, setCurrentCity] = useState({});
+    // Destructure the state variable
+    const [{ cities, isLoading, currentCity }, dispatch] = useReducer(
+        reducer,
+        initialState
+    );
 
     useEffect(() => {
         const fetchCities = async () => {
             try {
-                setIsLoading(true);
+                dispatch({ type: 'LOADING' });
 
                 const response = await fetch(`${BASE_URL}/cities`);
 
@@ -24,40 +62,47 @@ const CitiesContextProvider = ({ children }) => {
 
                 const data = await response.json();
 
-                setCities(data);
+                dispatch({ type: 'CITIES/LOADED', payload: data });
             } catch (error) {
+                // Error is not being used in this application
+                // Is better if we include this to follow the pattern in other apps and actually use the error there
                 console.error(error.message);
-            } finally {
-                setIsLoading(false);
+                dispatch({ type: 'REJECTED', payload: error.message });
             }
         };
 
         fetchCities();
     }, []);
 
-    const getCurrentCity = useCallback(async id => {
-        try {
-            setIsLoading(true);
-
-            const response = await fetch(`${BASE_URL}/cities/${id}`);
-
-            if (!response.ok) {
-                throw new Error('Error fetching city data.');
+    const getCurrentCity = useCallback(
+        async id => {
+            if (Number(id) === currentCity.id) {
+                return;
             }
 
-            const data = await response.json();
+            try {
+                dispatch({ type: 'LOADING' });
 
-            setCurrentCity(data);
-        } catch (error) {
-            console.error(error.message);
-        } finally {
-            setIsLoading(false);
-        }
-    }, []);
+                const response = await fetch(`${BASE_URL}/cities/${id}`);
+
+                if (!response.ok) {
+                    throw new Error('Error fetching city data.');
+                }
+
+                const data = await response.json();
+
+                dispatch({ type: 'CITY/LOADED', payload: data });
+            } catch (error) {
+                console.error(error.message);
+                dispatch({ type: 'REJECTED', payload: error.message });
+            }
+        },
+        [currentCity.id]
+    );
 
     const createCity = async newCity => {
         try {
-            setIsLoading(true);
+            dispatch({ type: 'LOADING' });
 
             const response = await fetch(`${BASE_URL}/cities/`, {
                 method: 'POST',
@@ -74,17 +119,16 @@ const CitiesContextProvider = ({ children }) => {
             const data = await response.json();
 
             // Once the backend data is modified we have to synchronize the client data too
-            setCities(prevCities => [...prevCities, data]);
+            dispatch({ type: 'CITY/CREATED', payload: data });
         } catch (error) {
             console.error(error.message);
-        } finally {
-            setIsLoading(false);
+            dispatch({ type: 'REJECTED', payload: error.message });
         }
     };
 
     const deleteCity = async id => {
         try {
-            setIsLoading(true);
+            dispatch({ type: 'LOADING' });
 
             const response = await fetch(`${BASE_URL}/cities/${id}`, {
                 method: 'DELETE',
@@ -95,14 +139,14 @@ const CitiesContextProvider = ({ children }) => {
             }
 
             // Once the backend data is modified we have to synchronize the client data too
-            setCities(prevCities => prevCities.filter(city => city.id !== id));
+            dispatch({ type: 'CITY/DELETED', payload: id });
         } catch (error) {
             console.error(error.message);
-        } finally {
-            setIsLoading(false);
         }
     };
 
+    // If we weren't dealing with asynchronous actions, we could've passed the dispatch function
+    // to the context value, and use it in the necessary components to dispatch actions
     const contextValue = {
         cities,
         isLoading,
